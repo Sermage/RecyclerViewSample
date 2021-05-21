2e6ef5f85220eac6087fd72354acd596f4358d6d
s.pisakov <s.pisakov@wnfx.ru>
2021-05-21 15:21:28 +0200

    Add item decoration

--- app/src/main/java/com/example/recyclerviewsample/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewsample/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.recyclerviewsample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +11,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val listOfItems = mutableListOf("First", "Second", "Third", "Forth", "Fifth")
+        val layoutManager=LinearLayoutManager(this)
         val adapter = Adapter(listOfItems)
         recyclerView.adapter = adapter
+        recyclerView.layoutManager=layoutManager
+        val itemDecoration=DividerItemDecoration(recyclerView.context,layoutManager.orientation)
+        recyclerView.addItemDecoration(itemDecoration)
 
     }
 }

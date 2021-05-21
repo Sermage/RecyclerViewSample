2ee4d311375c30c1929c8c4697fd19445282f685
s.pisakov <s.pisakov@wnfx.ru>
2021-05-21 12:51:54 +0200

    Some fixes

--- app/src/main/java/com/example/recyclerviewsample/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewsample/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.example.recyclerviewsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val listOfItems= mutableListOf("First","Second","Third","Forth","Fifth")
-        val recyclerView=recyclerView
-        val adapter= Adapter(listOfItems)
-        recyclerView.adapter=adapter
+        val listOfItems = mutableListOf("First", "Second", "Third", "Forth", "Fifth")
+        val adapter = Adapter(listOfItems)
+        recyclerView.adapter = adapter
 
     }
 }
